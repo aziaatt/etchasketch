77d178e307a1c81f0e06dda13c18d13a99aecb3e
aziaatt <aziaat@Xubuntu.myguest.virtualbox.org>
2023-02-27 15:03:22 +0100

    Finished the project.

---
+++ script.js
@@ -0,0 +1,25 @@
+const container = document.getElementById("container");
+const gridItem= document.getElementsByClassName("grid-item");
+
+makeRows(16,16);
+
+function makeRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    container.appendChild(cell).className = "grid-item";
+  };
+};
+
+container.addEventListener('mouseover', function classClicked(event){
+    event.target.classList.add('addedColor');
+});
+
+function amountGrids() {
+    let gridAmount = prompt("How many grids would you like?");
+    while (container.firstChild){
+        container.removeChild(container.lastChild);
+    }
+    makeRows(gridAmount, gridAmount);
+};
